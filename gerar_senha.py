21bd46855290185ee776dffb2e7cd56327185609
Samuel Kevin <72141979+samuelkev@users.noreply.github.com>
2022-08-20 02:18:13 -0400

    Update gerar_senha.py

--- gerar_senha.py
+++ gerar_senha.py
@@ -4,16 +4,16 @@ from tkinter import messagebox
 import pyperclip
 
 gui = Tk()
-gui.title('Gerador de Senha')
-gui.geometry('300x100')
+gui.title("Gerador de Senha")
+gui.geometry("300x100")
 gui.resizable(0,0)
 
 def gerar_senha():
     tamanho = int(passar_string.get())
 
-    letras_minusculas = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    letras_maisculas = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    numeros = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    letras_minusculas = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    letras_maisculas = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    numeros = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
     tudo = letras_minusculas + letras_maisculas + numeros
     aleatorio = random.sample(tudo, tamanho)
     senha = "".join(aleatorio)
